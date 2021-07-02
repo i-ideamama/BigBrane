7f04e74a1e4da20d592f8517d248e6824d6da66c
nandhagk <62976649+nandhagk@users.noreply.github.com>
2021-07-02 15:57:17 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,9 +1,12 @@
+from loggin import basicConfig, INFO
 from os import getenv
 
 from dotenv import load_dotenv
 
 from bot.bot import StarterBot
 
+
+basicConfig(level=INFO)
 load_dotenv()
 
 bot = StarterBot()
